9ddc6c2bcb51afb21a120f17634f67e132cfacb3
imad <imad-ak@hotmail.com>
2026-09-08 12:00:46 +0200

    Initial Commit

---
+++ LidlessTests/LidlessTests.swift
@@ -0,0 +1,19 @@
+//
+//  LidlessTests.swift
+//  LidlessTests
+//
+//  Created by imad on 08.09.26.
+//
+
+import Testing
+@testable import Lidless
+
+struct LidlessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
