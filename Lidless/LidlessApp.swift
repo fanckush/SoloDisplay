3e71ab0e687ae328496f612c246b51d16d5f89c2
imad <imad-ak@hotmail.com>
2026-09-08 12:00:46 +0200

    Initial commit

---
+++ Lidless/LidlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidlessApp.swift
+//  Lidless
+//
+//  Created by imad on 08.09.26.
+//
+
+import SwiftUI
+
+@main
+struct LidlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
